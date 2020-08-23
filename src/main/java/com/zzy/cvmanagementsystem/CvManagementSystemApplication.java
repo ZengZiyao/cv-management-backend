9caafbabf707a629e812c531ead2bcdacfa2bccd
Ziyao Zeng <zizeng9911@gmail.com>
2020-08-23 15:54:36 +0800

    Initialize spring boot porject

---
+++ src/main/java/com/zzy/cvmanagementsystem/CvManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zzy.cvmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzy/cvmanagementsystem/CvManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.cvmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
